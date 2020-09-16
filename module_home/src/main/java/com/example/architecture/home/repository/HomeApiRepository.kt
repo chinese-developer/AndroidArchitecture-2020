ad2a0109554cead02dc3892651b8dead2f044900
chinese-developer <privateemailmaster@gmail.com>
2020-09-16 14:24:25 +0800

    -

--- module_home/src/main/java/com/example/architecture/home/repository/HomeApiRepository.kt
+++ module_home/src/main/java/com/example/architecture/home/repository/HomeApiRepository.kt
@@ -10,28 +10,28 @@ import com.app.base.AppContext
 import com.app.base.app.BaseRepository
 import com.app.base.dagger.OKHTTP_REGULAR
 import com.app.base.dagger.OKHTTP_WITHOUT_TOKEN
-import com.app.base.data.api.AuthTokenLocalDataSource
 import com.app.base.data.api.NetResult
+import com.app.base.data.app.AppDataSource
 import com.app.base.data.models.NetEase
 import com.app.base.data.models.Song
+import com.app.base.utils.IOUtils.copyInputStreamToFile
+import com.app.base.utils.IOUtils.createFileToDownLoadDir
 import com.example.architecture.home.HomeApiService
 import com.example.architecture.home.repository.pojo.AlbumCoverImageUrlPojo
 import com.example.architecture.home.repository.pojo.AlbumListPojo
 import com.example.architecture.home.repository.pojo.LyricPojo
-import com.app.base.utils.IOUtils.copyInputStreamToFile
-import com.app.base.utils.IOUtils.createFileToDownLoadDir
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
 class HomeApiRepository @Inject constructor(
-        @Named(OKHTTP_REGULAR) private val api: HomeApiService,
-        @Named(OKHTTP_WITHOUT_TOKEN) private val apiWithoutToken: HomeApiService,
-        private val tokenSource: AuthTokenLocalDataSource
+    private val appDataSource: AppDataSource,
+    @Named(OKHTTP_REGULAR) private val api: HomeApiService,
+    @Named(OKHTTP_WITHOUT_TOKEN) private val apiWithoutToken: HomeApiService
 ) : BaseRepository() {
 
-    fun checkIfHasToken(): Boolean = tokenSource.checkIfHasToken()
+    fun checkIfHasToken(): Boolean = appDataSource.checkIfHasToken()
 
     suspend fun getLyric(id: Long): NetResult<LyricPojo> = safeApiCallWithoutCode {
         api.lyric(buildingLyricParams(id.toString()))
@@ -53,9 +53,10 @@ class HomeApiRepository @Inject constructor(
         api.syncAlbumList(buildingSyncAlbumListParams(uid))
     }
 
-    suspend fun getAlbumCoverImgUrlById(id: String?): NetResult<AlbumCoverImageUrlPojo> = safeApiCallWithoutCode {
-        api.getAlbumCoverImgUrlById(buildingAlbumCoverImgUrlParams(id))
-    }
+    suspend fun getAlbumCoverImgUrlById(id: String?): NetResult<AlbumCoverImageUrlPojo> =
+        safeApiCallWithoutCode {
+            api.getAlbumCoverImgUrlById(buildingAlbumCoverImgUrlParams(id))
+        }
 
     suspend fun download(url: String): NetResult<String> {
         val response = apiWithoutToken.download(url)
@@ -69,10 +70,10 @@ class HomeApiRepository @Inject constructor(
         }
 
         return NetResult.Error(
-                ErrorException(
-                        response.code(),
-                        response.message()
-                )
+            ErrorException(
+                response.code(),
+                response.message()
+            )
         )
     }
 
@@ -80,14 +81,14 @@ class HomeApiRepository @Inject constructor(
         val user = AppContext.get().appDataSource.user()
         return if (user?.displayName != null) {
             mapOf(
-                    "types" to "userlist",
-                    "uid" to uid,
-                    "username" to user.displayName!!
+                "types" to "userlist",
+                "uid" to uid,
+                "username" to user.displayName!!
             )
         } else {
             mapOf(
-                    "types" to "userlist",
-                    "uid" to uid
+                "types" to "userlist",
+                "uid" to uid
             )
         }
 
@@ -95,39 +96,39 @@ class HomeApiRepository @Inject constructor(
 
     private fun buildingAlbumCoverImgUrlParams(id: String?): Map<String, String?> {
         return mapOf(
-                "types" to "pic",
-                "id" to id,
-                "source" to "netease"
+            "types" to "pic",
+            "id" to id,
+            "source" to "netease"
         )
     }
 
     private fun buildingSongUrlParams(id: String): Map<String, String> {
         return mapOf(
-                "types" to "url",
-                "id" to id,
-                "source" to "netease"
+            "types" to "url",
+            "id" to id,
+            "source" to "netease"
         )
     }
 
     private fun buildingLogoutParams(): Map<String, String?> {
         return mapOf(
-                "token" to AppContext.appDataSource().appToken(),
-                "types" to "logout"
+            "token" to AppContext.appDataSource().appToken(),
+            "types" to "logout"
         )
     }
 
     private fun buildingAlbumListParams(id: String): Map<String, String> {
         return mapOf(
-                "types" to "playlist",
-                "id" to id
+            "types" to "playlist",
+            "id" to id
         )
     }
 
     private fun buildingLyricParams(id: String): Map<String, String> {
         return mapOf(
-                "types" to "lyric",
-                "id" to id,
-                "source" to "netease"
+            "types" to "lyric",
+            "id" to id,
+            "source" to "netease"
         )
     }
 }
